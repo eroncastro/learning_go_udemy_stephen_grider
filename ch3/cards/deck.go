c3597059323efc55b054d4f04528e9df8e0137d0
Eron Castro <eroncastro@gmail.com>
2021-03-10 10:15:44 -0300

    Add NewDeck method

--- ch3/cards/deck.go
+++ ch3/cards/deck.go
@@ -1,10 +1,29 @@
-package main
+package cards
 
 import "fmt"
 
-type deck []string
+// Deck extends type string
+type Deck []string
 
-func (d deck) print() {
+var suits [4]string = [4]string{"Spades", "Clubs", "Diamonds", "Hearts"}
+var values [13]string = [13]string{
+	"Ace", "Two", "Three", "Four", "Five", "Six",
+	"Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King",
+}
+
+func NewDeck() Deck {
+	cards := Deck{}
+
+	for _, suit := range suits {
+		for _, value := range values {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d Deck) Print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
