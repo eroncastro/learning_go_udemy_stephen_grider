9dc5554b802ea2c0f53c08f77120ad66df8fa7fc
Eron Castro <eroncastro@gmail.com>
2021-03-21 12:19:24 -0300

    Add embedded structs

--- ch4/main.go
+++ ch4/main.go
@@ -5,6 +5,12 @@ import "fmt"
 type Person struct {
 	FirstName string
 	LastName  string
+	Contact   ContactInfo
+}
+
+type ContactInfo struct {
+	Email   string
+	ZipCode int
 }
 
 func main() {
@@ -17,5 +23,18 @@ func main() {
 	fmt.Printf("%+v\n", p)
 
 	var p2 Person
+	p2.Contact.Email = "lala@lala.com"
+	p2.Contact.ZipCode = 12345
 	fmt.Printf("%+v\n", p2)
+
+	p3 := Person{
+		FirstName: "Jim",
+		LastName:  "Anderson",
+		Contact: ContactInfo{
+			Email:   "jimanderson@email.com",
+			ZipCode: 12345,
+		},
+	}
+
+	fmt.Printf("%+v\n", p3)
 }
