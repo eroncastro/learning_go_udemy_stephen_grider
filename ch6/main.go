b38f95fbe790e8628d4c5c68065d852241db227d
Eron Castro <eroncastro@gmail.com>
2021-04-02 11:56:24 -0300

    Add interface lesson

---
+++ ch6/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// Interface type -> abstract
+type GreetingBot interface {
+	Greet(string) string
+}
+
+// Concrete type
+type EnglishBot struct{}
+
+func (EnglishBot) Greet(name string) string {
+	return "Hello, " + name
+}
+
+// Concrete type
+type SpanishBot struct{}
+
+func (SpanishBot) Greet(name string) string {
+	return "Hola, " + name
+}
+
+func printGreeting(g GreetingBot, name string) {
+	fmt.Println(g.Greet(name))
+}
+
+func main() {
+	englishBot := EnglishBot{}
+	spanishBot := SpanishBot{}
+
+	printGreeting(englishBot, "John")
+	printGreeting(spanishBot, "John")
+}
