c45476638e6944b4509c1aa2a9da5a16734936c5
Eron Castro <eroncastro@gmail.com>
2021-04-28 09:23:10 -0300

    Add checkLink

---
+++ ch7/basics/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+
+	if err != nil {
+		fmt.Println(link + " might be down.")
+	} else {
+		fmt.Println(link + " is up.")
+	}
+
+	c <- link
+}
+
+func main() {
+	links := []string{
+		"https://google.com",
+		"https://amazon.com",
+		"https://facebook.com",
+		"https://stackoverflow.com",
+	}
+	c := make(chan string, 2)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for {
+		go checkLink(<-c, c)
+	}
+}
